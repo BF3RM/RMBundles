324cda29bde903f949605e09fa3f0f9188c5aa54
Ori <buhbutoriasher@gmail.com>
2024-08-04 11:08:59 +0300

    - update submodule
    - remove unused functions

--- ext/Shared/__init__.lua
+++ ext/Shared/__init__.lua
@@ -11,8 +11,6 @@ function RMBundles:__init()
 
 	Hooks:Install('EntityFactory:CreateFromBlueprint', 901, self, self.OnEntityCreateFromBlueprint)
 	Hooks:Install('ResourceManager:LoadBundles', 901, self, self.OnLoadBundles)
-	Events:Subscribe('Level:RegisterEntityResources', self, self.OnRegisterEntityResources)
-	Events:Subscribe('Level:LoadResources', self, self.OnLoadResources)
 end
 
 function RMBundles:OnEntityCreateFromBlueprint(p_Hook, p_Blueprint, p_Transform, p_Variation, p_ParentRepresentative)
@@ -35,16 +33,5 @@ function RMBundles:OnLoadBundles(p_Hook, p_Bundles, p_Compartment)
 	m_Logger:Write("OnLoadBundles")
 	m_BundleLoader:OnLoadBundles(p_Hook, p_Bundles, p_Compartment)
 end
-
-function RMBundles:OnRegisterEntityResources(p_LevelData)
-	m_Logger:Write("OnRegisterEntityResources")
-	m_BundleLoader:OnRegisterEntityResources(p_LevelData)
-end
-
-function RMBundles:OnLoadResources(p_MapName, p_GameModeName, p_DedicatedServer)
-	m_Logger:Write("OnLoadResources")
-	m_BundleLoader:OnLoadResources(p_MapName, p_GameModeName, p_DedicatedServer)
-end
-
 return RMBundles()
 
